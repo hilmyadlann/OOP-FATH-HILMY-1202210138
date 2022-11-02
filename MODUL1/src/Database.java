a13b218e01c372317ab6bf991db3092f91a9e9fb
hilmyadlann <hilmyadlann@gmail.com>
2022-11-02 16:29:58 +0700

    Feat:MODUL1

---
+++ MODUL1/src/Database.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    public void CreateMenu()
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    public void InsertMenu()
+    // TODO Create Method to Insert Menu to Database
+    public void ShowMenu()
+    // TODO Create Method to Show Menu from Database
+    public void SearchMenu()
+    // TODO Create Method to Search Menu from Database
+
+
+}
